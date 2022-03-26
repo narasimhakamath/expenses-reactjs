358243a2888fb3aec364ff974e193685245b62f0
Narasimha Kamath <narasimhakamath@outlook.com>
2022-03-26 15:37:51 +0530

    Created components to view hardcoded expense list.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Expenses from "./components/Expenses";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const expenses = [
+		{id: 1, title: 'Train ticket', amount: 1200, date: new Date(2022, 2, 25)},
+		{id: 2, title: 'Dinner', amount: 200, date: new Date(2022, 1, 7)},
+		{id: 3, title: 'Smirnoff Green Apple', amount: 960, date: new Date(2022, 11, 15)},
+		{id: 4, title: 'Uber ride', amount: 300, date: new Date(2022, 9, 3)}
+	];
+
+	return (
+		<div>
+			<h2>Let's get started</h2>
+			<Expenses items={expenses} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+
+const ExpenseItem = props => {
+	return (
+		<Card className="expense-item">
+			<ExpenseDate date={props.date} />
+			<div className="expense-item__description">
+				<h2>{props.title}</h2>
+			</div>
+			<div className="expense-item__price">₹ {props.amount}</div>
+		</Card>
+	);
+}
+
+export default ExpenseItem;
